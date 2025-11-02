d6f31296e9ef1e0dd1cf2ee3c85797021122240b
Seaszn <111234706+seaszn@users.noreply.github.com>
2025-11-02 05:27:36 +0100

    fixes

--- web/react-router.config.ts
+++ web/react-router.config.ts
@@ -1,14 +1,14 @@
 import type { Config } from "@react-router/dev/config";
 
 const arg = process.argv.find(arg => arg.startsWith('--mode='))
-const mode = arg?.split('=')[1] || 'store'
+const mode = arg?.split('=')[1] || 'public'
 
-if(!['store' , 'admin'].includes(mode)){
+if(!['public' , 'admin'].includes(mode)){
   throw new Error(`Invalid runtime mode: ${mode}`)
 }
 
 export default {
   ssr: true,
-  appDirectory: `./lib/store`,
-  // buildDirectory: `lib/${mode}`
+  appDirectory: `./lib/${mode}`,
+  buildDirectory: `./build/${mode}`,
 } satisfies Config;
