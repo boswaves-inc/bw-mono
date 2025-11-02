bd4be4ece9704bb42bd506efca8a6871c5c396dd
Seaszn <111234706+seaszn@users.noreply.github.com>
2025-11-02 05:26:35 +0100

    t

--- web/lib/store copy/components/types.ts
+++
@@ -1,6 +0,0 @@
-import type { DetailedHTMLProps, HTMLAttributes, PropsWithChildren } from "react"
-
-export type HeadlessProps<T> = Omit<T, 'className' | 'children'> & PropsWithChildren<{ className?: string }>
-
-export type DivAttr = HTMLAttributes<HTMLDivElement>
-export type DivProps = DetailedHTMLProps<DivAttr, HTMLDivElement>
--- web/lib/store/routes.ts
+++ web/lib/store/routes.ts
@@ -1,18 +1,4 @@
-import { route, type RouteConfig } from "@react-router/dev/routes";
+import { type RouteConfig } from "@react-router/dev/routes";
 import { flatRoutes } from "@react-router/fs-routes";
-import { existsSync } from "node:fs";
-import path from "node:path";
 
-const router = async ({ directory }: { directory: `./${string}` }): Promise<RouteConfig> => {
-    const content = await flatRoutes({ rootDirectory: `${directory}/routes`, });
-
-    if (existsSync(path.resolve(`${directory}/root.tsx`))) {
-        return [route('/', `${directory}/root.tsx`, content)]
-    }
-
-    throw new Error(`bundle '${directory}/root.tsx' does not exist `)
-}
-
-const store = await router({ directory: './store' })
-
-export default store satisfies RouteConfig
+export default await flatRoutes({}) satisfies RouteConfig
