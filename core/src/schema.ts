31c827f000d934d2a2380cc9b70375fb5e315a61
Seaszn <111234706+seaszn@users.noreply.github.com>
2025-11-03 20:16:50 +0100

    fixes

---
+++ core/src/schema.ts
@@ -0,0 +1,10 @@
+import { Item, ItemPrice, ItemType, PeriodUnit } from './schema/item'
+import { User, UserProvider, UserCredentials } from './schema/user'
+
+export {
+    Item, ItemPrice, ItemType, PeriodUnit
+}
+
+export default {
+    Item, ItemPrice, ItemType, PeriodUnit
+}
