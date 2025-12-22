d451131c7fd4b1b8fc8ee446ee500a5fba358fdf
Seaszn <111234706+seaszn@users.noreply.github.com>
2025-12-22 17:58:46 +0100

    fixecd compnay page

--- apps/web/app/components/v3/container.tsx
+++ apps/web/app/components/v3/container.tsx
@@ -3,7 +3,7 @@ import { cn } from '~/utils/class'
 
 export function Container({ className, children, }: ComponentProps<'div'>) {
     return (
-        <div className={cn(className, 'px-6 lg:px-8')}>
+        <div className={cn('px-6 lg:px-8', className)}>
             <div className="mx-auto max-w-2xl lg:max-w-7xl">
                 {children}
             </div>
